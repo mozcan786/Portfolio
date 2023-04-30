0bb8e8a9472d4207610ba863125ed6f8640b3248
muhammed ozcan <muhammedozcan786@gmail.com>
2023-04-30 14:35:10 +0300

    Seo 100%

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
     <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
       <div className='container mx-auto'>
         <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl  text-white/50'>
-          <Link to='home'
+          <Link name='home'  to='home'
             activeClass='active'
             smooth={true}
             spy={true}
@@ -16,27 +16,27 @@ const Nav = () => {
             className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
             <BiHomeAlt />
           </Link>
-          <Link to='about'
+          <Link name='about'  to='about'
             activeClass='active'
             smooth={true}
             spy={true}
             className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
             <BiUser />
           </Link>
-          <Link to='services'
+          <Link name='services'  to='services'
             activeClass='active'
             smooth={true}
             spy={true}
             className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
             <BsClipboardData />
           </Link>
-          <Link to='work'
+          <Link name='work'  to='work'
             activeClass='active'
             smooth={true} spy={true}
             className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
             <BsBriefcase />
           </Link>
-          <Link to='contact'
+          <Link name='contact'  to='contact'
             activeClass='active'
             smooth={true} spy={true}
             className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
